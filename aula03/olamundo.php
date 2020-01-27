8d2ab32e82fca324fb6acc403ca6fe5fccfb598c
Natanael Saymon <nsaymonsouza@gmail.com>
2020-01-27 16:15:58 -0300

    iniciando estudos em PHP

---
+++ aula03/olamundo.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+
+<html lang="pt-br">
+    <head>
+        <title>Ola mundo</title>
+        <meta charset="utf-8">
+
+        <style>
+            *{
+                font-family: arial;
+            }
+        </style>
+    </head>
+
+    <body>
+        <h1>Testando PHP</h1>
+
+        <?php 
+            echo "Olá, Mundo!";
+        ?>
+    </body>
+</html>
---
+++ aula04/variaveis.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+
+<html lang="pt-br">
+    <head>
+        <title>Ola mundo</title>
+        <meta charset="utf-8">
+
+        <style>
+            *{
+                font-family: arial;
+            }
+        </style>
+    </head>
+
+    <body>
+        <h1>Variaveis em PHP</h1>
+
+        <?php 
+            $numero = 28;
+            $nome = "Natanael";
+            echo $nome, $numero;
+        ?>
+    </body>
+</html>
